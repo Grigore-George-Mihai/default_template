a20671d2ca904726cf113ea8d1c829e1d938993e
George Mihai Grigore <grigore.george.mihaii@gmail.com>
2024-08-18 13:05:47 +0300

    Add UserDecorator and a common method

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_09_101614) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
